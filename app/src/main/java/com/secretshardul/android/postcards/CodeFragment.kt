da68d262068e598c17981139ca1c919783695d87
shardul <shardul.aeer@somaiya.edu>
2021-01-21 22:26:18 +0530

    feat: Display formatted code for CURL

--- app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
+++ app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
@@ -9,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import io.github.kbiakov.codeview.CodeView
+import io.github.kbiakov.codeview.adapters.Options
+import io.github.kbiakov.codeview.highlight.ColorTheme
 
 class CodeFragment : Fragment() {
 
@@ -21,20 +24,21 @@ class CodeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_code, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val apiKeyView: TextView = view.findViewById(R.id.api_key)
+        val codeView: CodeView = view.findViewById(R.id.code_view)
+        codeView.setOptions(
+            Options.Default.get(view.context)
+                .withLanguage("sh")
+        )
 
         this.activity?.let { activity ->
             val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
             val savedDocumentId = sharedPref.getString(MainActivity.DOCUMENT_ID_KEY, null)
 
             apiKeyView.text = savedDocumentId
+            codeView.setCode("curl -X POST https://app.com \\\n\t--d 'key:${savedDocumentId}'")
         }
 
     }

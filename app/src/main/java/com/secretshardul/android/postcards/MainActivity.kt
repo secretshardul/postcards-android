b49fc2db70f526b756ed890d357701beb7ce9b08
shardul <shardul.aeer@somaiya.edu>
2021-01-21 19:16:04 +0530

    feat: Notification channel with custom sound

--- app/src/main/java/com/secretshardul/android/postcards/MainActivity.kt
+++ app/src/main/java/com/secretshardul/android/postcards/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.secretshardul.android.postcards
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentResolver
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.ktx.Firebase
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +32,35 @@ class MainActivity : AppCompatActivity() {
             return@setOnNavigationItemSelectedListener true
         }
         bottomNavigation.selectedItemId = R.id.navigation_postcards
+
+        // Notification channel with custom sound
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val sound = Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + baseContext.packageName + "/" + R.raw.office_phone
+            )
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+
+            val channel = NotificationChannel(
+                baseContext.getString(R.string.default_notification_channel_id),
+                "Default alarm channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "Notification with long sound alert"
+            channel.enableLights(true)
+            channel.enableVibration(true)
+            channel.setSound(sound, audioAttributes)
+
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            ) as NotificationManager
+            Timber.d("Existing channels: ${notificationManager.notificationChannels}")
+            notificationManager.createNotificationChannel(channel)
+
+            Timber.d("Channel created")
+        }
 
     }
 
--- app/src/main/java/com/secretshardul/android/postcards/NotificationService.kt
+++ app/src/main/java/com/secretshardul/android/postcards/NotificationService.kt
@@ -1,10 +1,22 @@
 package com.secretshardul.android.postcards
 
+import android.R
+import android.app.NotificationManager
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
+
 class NotificationService: FirebaseMessagingService() {
+//    val NOTIFICATION_SOUND_URI=
+//        Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + BuildConfig.APPLICATION_ID + "/" + R.raw.telegraph)
+
+    val VIBRATE_PATTERN = longArrayOf(0, 500)
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Timber.d("Got message: $message")
@@ -13,5 +25,12 @@ class NotificationService: FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
         Timber.d("Got new token $newToken")
+
+        val notification = NotificationCompat.Builder(baseContext)
+            .setVibrate(VIBRATE_PATTERN)
+            .build()
+
+        val notificationManager = baseContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
     }
 }

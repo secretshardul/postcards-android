2a273c654b424417c0dcd7e9f84449129f5e1cd1
shardul <shardul.aeer@somaiya.edu>
2021-01-21 21:42:09 +0530

    feat: Display API key

--- app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
+++ app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
@@ -1,11 +1,13 @@
 package com.secretshardul.android.postcards
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 
 class CodeFragment : Fragment() {
@@ -21,6 +23,20 @@ class CodeFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val apiKeyView: TextView = view.findViewById(R.id.api_key)
+
+        this.activity?.let { activity ->
+            val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+            val savedDocumentId = sharedPref.getString(MainActivity.DOCUMENT_ID_KEY, null)
+
+            apiKeyView.text = savedDocumentId
+        }
+
     }
 
 }

8f521eff16f46ade80125765809986124c0e2271
shardul <shardul.aeer@somaiya.edu>
2021-01-24 16:03:40 +0530

    feat: Copy buttons for API key and CURL code

--- app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
+++ app/src/main/java/com/secretshardul/android/postcards/CodeFragment.kt
@@ -1,17 +1,21 @@
 package com.secretshardul.android.postcards
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import io.github.kbiakov.codeview.CodeView
 import io.github.kbiakov.codeview.adapters.Options
-import io.github.kbiakov.codeview.highlight.ColorTheme
+
 
 class CodeFragment : Fragment() {
 
@@ -28,6 +32,9 @@ class CodeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val apiKeyView: TextView = view.findViewById(R.id.api_key)
         val codeView: CodeView = view.findViewById(R.id.code_view)
+        val copyCodeButton: Button = view.findViewById(R.id.copy_code_button)
+        val copyKeyButton: Button = view.findViewById(R.id.copy_key_button)
+
         codeView.setOptions(
             Options.Default.get(view.context)
                 .withLanguage("sh")
@@ -38,14 +45,31 @@ class CodeFragment : Fragment() {
             val savedDocumentId = sharedPref.getString(MainActivity.DOCUMENT_ID_KEY, null)
 
             apiKeyView.text = savedDocumentId
-            val curlCode = "curl -G -X  POST 'https://postcards-api-notifications.web.app/api'\\\n" +
-                    "\t--data-urlencode 'key=${savedDocumentId}'\\\n" +
-                    "\t--data-urlencode 'title=hello world'\\\n" +
-                    "\t--data-urlencode 'body=hello frompostcards'\\\n" +
-                    "\t--data-urlencode 'imageUrl=https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg' # optional"
-            codeView.setCode(curlCode)
-        }
 
+            val curlCode =
+                "curl -G -X  POST 'https://postcards-api-notifications.web.app/api'\\\n" +
+                        "\t--data-urlencode 'key=${savedDocumentId}'\\\n" +
+                        "\t--data-urlencode 'title=hello world'\\\n" +
+                        "\t--data-urlencode 'body=hello frompostcards'\\\n" +
+                        "\t--data-urlencode 'imageUrl=https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg' # optional"
+            codeView.setCode(curlCode)
+
+            val clipboard = getSystemService(view.context, ClipboardManager::class.java)
+            copyKeyButton.setOnClickListener {
+                val clip = ClipData.newPlainText("postcards-api-key", savedDocumentId)
+                clipboard?.setPrimaryClip(clip)
+                displayCopiedTextMessage(view.context)
+            }
+
+            copyCodeButton.setOnClickListener {
+                val clip = ClipData.newPlainText("postcards-curl-code", curlCode)
+                clipboard?.setPrimaryClip(clip)
+                displayCopiedTextMessage(view.context)
+            }
+        }
     }
 
+    private fun displayCopiedTextMessage(context: Context) {
+        Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+    }
 }
